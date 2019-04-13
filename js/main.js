ec5637fc8b16ff4487962dbf244936fe5eb94a92
sisatata <sadiqul842@gmail.com>
2019-04-13 11:16:41 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ var checkPass = "";
 var loginSuccess = false;
 var wrong = "Wrong User Name Or Password ";
 var chooseOption;
-var count = -1;
+var count = 0;
 var questionHeading = "Question Number:";
-var answered = false;
-var score;
+
+var score = 0;
 var answered = [];
+
+
 var optionDescription;
 
 function toggleResetPswd(e) {
@@ -48,7 +50,7 @@ var users = [
     }
 
 ];
-
+/*Sign In Page*/
 //  Check username and password
 function checkValidation() {
     var i, j;
@@ -75,7 +77,13 @@ function checkValidation() {
     }
 
 }
+/*Option Pages*/
+//function to go js quiz page
+function  javascript() {
 
+    window.location="quiz.html";
+
+}
 
 var questions = [
 
@@ -104,77 +112,77 @@ var questions = [
     {
         question: "How does Java Script store dates in objects of Date type?",
         options: ["A: The number of days since January 1st, 1900", "B: he number of seconds since January 1st, 1970", "C: The number of milliseconds since January 1st, 1970", "D: The number of picoseconds since January 1st, 1970"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "Which of the following is the tainted property of a window object in Java Script?",
         options: ["A: Pathname", "B: Protocol", "C:  Defaultstatus", "D: Host"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "Which attribute needs to be changed to make elements invisible?",
         options: ["A:  visibilty", "B: visible", "C:  invisibility", "D: invisible"],
-        answer:1
+        answer: 1
 
 
     },
     {
         question: "Which of the following is not a valid JavaScript variable name?",
         options: ["A: 2java", "B:  _java_and_ java _names", "C: javaandjava", "D: None of the above"],
-        answer:1
+        answer: 1
 
 
     },
     {
         question: "Which of the ways below is incorrect of instantiating a date?",
         options: ["A: new Date(dateString)", "B: new Date()", "C: new Date(seconds)", "D: new Date(year, month, day, hours, minutes, seconds, milliseconds)"],
-        answer:3
+        answer: 3
 
 
     },
     {
         question: "___________ JavaScript is also called client-side JavaScript.",
         options: ["A: Microsoft", "B:  Navigator", "C: LiveWire", "D: Native"],
-        answer:2
+        answer: 2
 
 
     },
     {
         question: "What language defines the behavior of a web page?",
         options: ["A: HTML", "B: CSS", "C:  XML", "D: Java Script"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "What is the alternate name for Java script?",
         options: ["A: LimeScript", "B:  Both a and d", "C: ECMScript", "D: ECMAScript"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "Which of the following is a client-side Java Script object?",
         options: ["A:  File", "B: Function", "C: FileUpload", "D: Time"],
-        answer:3
+        answer: 3
 
 
     },
     {
         question: "What java wrapper type is created when a JavaScript object is sent to Java?",
         options: ["A:ScriptObject", "B: JavaObject", "C:  Jobject", "D: JSObject"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "Which of the following method is used to evaluate a string of Java Script code in the context of the specified object?",
         options: ["A: Eval", "B: ParseDoule", "C: ParseObject", "D: Efloat"],
-        answer:1
+        answer: 1
 
 
     },
@@ -188,35 +196,35 @@ var questions = [
     {
         question: "Why Java and JavaScript have similar name?",
         options: ["A: Java Script is a stripped-down version of Java", "B: The syntax of JavaScript is loosely based on Java syntax", "C:They both support Object Oriented Programming", "D: None of the above"],
-        answer:2
+        answer: 2
 
 
     },
     {
         question: "Which of the following are capable of JavaScript functions?",
         options: ["A: Returning multiple values", "B: Accepting parameters and returning values", "C: Accepting parameters", "D: all of the above"],
-        answer:4
+        answer: 4
 
 
     },
     {
         question: "Which attribute is used to hold JavaScript version?",
         options: ["A: SCRIPT", "B: VERSION", "C: LANGUAGE", "D: VER"],
-        answer:3
+        answer: 3
 
 
     },
     {
         question: "What type of image maps could be used with JavaScript?",
         options: ["A: Client-side image maps", "B: Server-side image maps", "C:  Both A and B", "D: Localhost image maps"],
-        answer:1
+        answer: 1
 
 
     },
     {
         question: "JavaScript entities start with ____________ and end with _____________",
         options: ["A: Semicolon, colon", "B: Semicolon, Ampersand", "C: Ampersand, colon", "D: Ampersand, semicolon"],
-        answer:4
+        answer: 4
 
 
     }
@@ -226,15 +234,10 @@ var questions = [
 
 //function to go to next question and reset
 
-function increase() {
+function onLoad() {
 
-    clear();
-    count++;
-    if(count>=20){
-        document.getElementById("next-btn").disabled = true;
-    }
-    var questionDescription;
-    answered = false;
+    checkPage();
+
 
     document.getElementById("questionNo").innerHTML = questionHeading + (count + 1);
     questionDescription = questions[count].question;
@@ -246,43 +249,20 @@ function increase() {
         questionDescription[i].innerHTML = questions[count].options[i];
 
     }
-
-    checkAnswer();
-}
-
-// function adopting answred
-function checkAnswer() {
-
-
-    var iterate = document.getElementsByClassName("options");
-
+    // checkAnswer();
     storeAnswer();
 
-    for (var i = 0; i < iterate.length; i++) {
-
-        iterate[i].addEventListener("click", function () {
-
-
-            clear();
-            this.style.color = "orange";
-            answered = true;
-
-
-        });
-
-    }
-
-
 }
+
 
 //function to clear option checked before
 
-function clear() {
+function reset() {
 
     questionDescription = document.getElementsByClassName("options");
     for (var i = 0; i < 4; i++) {
 
-        questionDescription[i].style.color = "unset";
+        questionDescription[i].style.color = "white";
 
 
     }
@@ -296,33 +276,221 @@ function storeAnswer() {
     var indx2 = document.getElementById("2");
     var indx3 = document.getElementById("3");
     var indx4 = document.getElementById("4");
-    indx1.addEventListener("click", function () {
+    reset();
+
+    document.getElementById("1").onclick = function () {
+
+
         answered[count] = 1;
+        reset();
+        indx1.style.color = "orange";
 
-    });
-    indx2.addEventListener("click", function () {
+
+    };
+    document.getElementById("2").onclick = function () {
+
+        reset();
         answered[count] = 2;
+        indx2.style.color = "orange";
 
-    });
-    indx3.addEventListener("click", function () {
+
+    };
+    document.getElementById("3").onclick = function () {
+
+        reset();
         answered[count] = 3;
+        indx3.style.color = "orange";
 
-    });
-    indx4.addEventListener("click", function () {
 
+    };
+    document.getElementById("4").onclick = function () {
+
+        reset();
         answered[count] = 4;
+        indx4.style.color = "orange";
+
+
+    };
+
+
+}
+
+// function to check if answered
+function isAnswered() {
+
+
+    if (answered[count] === 1) {
+        reset();
+        document.getElementById("1").style.color = "orange";
+
+
+    } else if (answered[count] === 2) {
+        reset();
+        document.getElementById("2").style.color = "orange";
+
+
+    } else if (answered[count] === 3) {
+        reset();
+        document.getElementById("3").style.color = "orange";
+
+
+    } else if (answered[count] === 4) {
+        reset();
+        document.getElementById("4").style.color = "orange";
+
+
+    } else {
+        reset();
+    }
+
+
+}
+
+// function to next page
+function increase() {
+
+
+    count++;
+
+    checkPage();
+    if (count >= 19) {
+        document.getElementById("next-btn").disabled = true;
+    }
+    var questionDescription;
+
+
+    document.getElementById("questionNo").innerHTML = questionHeading + (count + 1);
+    questionDescription = questions[count].question;
+    document.getElementById("question").innerHTML = questionDescription;
+    document.getElementsByClassName("options");
+    questionDescription = document.getElementsByClassName("options");
+    for (var i = 0; i < 4; i++) {
+
+        questionDescription[i].innerHTML = questions[count].options[i];
+
+    }
+
+    // checkAnswer();
+    isAnswered();
+
+
+}
+
+// function to previous page
+function decrease() {
+    checkPage();
+
+    count--;
+    checkPage();
+
+
+    var questionDescription;
+
+
+    document.getElementById("questionNo").innerHTML = questionHeading + (count + 1);
+    questionDescription = questions[count].question;
+    document.getElementById("question").innerHTML = questionDescription;
+    document.getElementsByClassName("options");
+    questionDescription = document.getElementsByClassName("options");
+    for (var i = 0; i < 4; i++) {
+
+        questionDescription[i].innerHTML = questions[count].options[i];
+
+    }
+
+    isAnswered();
+
+}
+
+// function for calculationg result
+function result() {
+
+    var i = 0;
+    for (i = 0; i < 20; i++) {
+        if (answered[i] === questions[i].answer) {
+            score++;
+        }
+    }
+    count = 0;
+    displaySet();
+
+
+    var action = document.getElementsByClassName("questionDiv");
+    for (i = 0; i < action.length; i++) {
+        action[i].style.display = "none";
+    }
+
+    score = score * 5;
+    document.getElementById("result").innerHTML = "Your Score: " + score;
+    if (score >= 90) {
+        document.getElementById("verdict").innerHTML = "Excellent";
+    } else if (score >= 80) {
+        document.getElementById("verdict").innerHTML = "Well Done";
+    } else if (score >= 60) {
+        document.getElementById("verdict").innerHTML = "Good";
+    } else if (score >= 40) {
+        document.getElementById("verdict").innerHTML = "Poor";
+    } else {
+        document.getElementById("verdict").innerHTML = "Fail";
+    }
+    score = 0;
+    answered = [];
+    count = 1;
+    var retakeExam = document.getElementById("retake-btn");
+    retakeExam.addEventListener("click", function () {
+        displayUnset();
+
 
     });
 
 
 }
 
+function displaySet() {
+
+    document.getElementById("retake-btn").style.display = "block";
+    document.getElementById("submit-btn").style.display = "none";
+    document.getElementById("result").style.display = "block";
+    document.getElementById("verdict").style.display = "block";
+    document.getElementById("next-btn").style.display = "none";
+    document.getElementById("prev-btn").style.display = "none";
+
+}
 
 
+function displayUnset() {
+    reset();
+    count = 0;
+    action = document.getElementsByClassName("questionDiv");
+    for (i = 0; i < action.length; i++) {
+        action[i].style.display = "block";
+    }
+    action = document.getElementsByTagName("button");
+
+    action[0].style.display = "block";
+    action[2].style.display = "block";
+    action[1].textContent = "Next";
+    document.getElementById("result").style.display = "none";
+    document.getElementById("verdict").style.display = "none";
+    document.getElementById("retake-btn").style.display = "none";
+    document.getElementById("next-btn").style.display = "block";
+    document.getElementById("prev-btn").style.display = "block";
+    document.getElementById("submit-btn").style.display = "block";
+    document.getElementById("next-btn").disabled = false;
+
+}
 
 
+function checkPage() {
+    if (count < 1) {
+        document.getElementById("prev-btn").disabled = true;
+    } else {
+        document.getElementById("prev-btn").disabled = false;
+
+    }
 
 
+}
 
 
 
