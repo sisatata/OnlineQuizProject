5e41ed394942e563482179ac6b7443646959a3c7
sisatata <sadiqul842@gmail.com>
2019-04-17 14:09:09 +0600

    form updated

--- js/main.js
+++ js/main.js
@@ -26,17 +26,10 @@ var totsum = 0;
 function toggleResetPswd(e) {
     e.preventDefault();
     $('#logreg-forms .form-signin').toggle() // display:block or none
-    $('#logreg-forms .form-reset').toggle() // display:block or none
+
 }
 
 
-$(() => {
-    // Login Register Form
-    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
-    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
-    $('#logreg-forms #btn-signup').click(toggleSignUp);
-    $('#logreg-forms #cancel_signup').click(toggleSignUp);
-})
 
 // Database for users
 var users = [
